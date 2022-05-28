0af26a2761082d4bc99bea7e0d585dc7c9b2ad87
NikhilPrajith <57269214+NikhilPrajith@users.noreply.github.com>
2022-05-27 22:34:29 -0400

    full

--- app/src/main/java/com/example/finalproject/GameActivity.java
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -1,13 +1,117 @@
 package com.example.finalproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class GameActivity extends AppCompatActivity {
-
+    public Data A;
+    public Data B;
+    public PlayGame game;
+    public Boolean gameEnd;
+    public String type;
+    public String beforeData = "";
+    public String afterData = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        Intent i = getIntent();
+        String passed = i.getExtras().getString("buttonId");
+        TextView name = (TextView) findViewById(R.id.mode);
+        type = passed;
+        name.setText(passed);
+        startGame(passed);
+
     }
+    public void startGame(String mode){
+        FileProcess a = new FileProcess(mode,getApplicationContext());
+        Map<Integer, Data> temp = a.getFiltered();
+        if(mode.equals("Networth") || mode.equals("Grocery")){
+            beforeData = "$";
+        }
+        if(mode.equals("Youtube")){
+            afterData = " subs";
+        }
+        if(mode.equals("Heights")){
+            afterData = "in";
+        }
+        if(temp.size()!=0){
+            game = new PlayGame(temp);
+            A = game.getASingleStartingData();
+            B = game.getComparingData(A);
+            gameEnd = false;
+            setData();
+        }
+
+    }
+    public void processUserChoice(Boolean result){
+        if(result == false){
+            System.out.println("Failed");
+            A = new Data("",0.0,"");
+            B = new Data("",0.0,"");
+        }else{
+            A = B;
+            B = game.getComparingData(A);
+
+        }
+        System.out.println("new Process");
+        setData();
+    }
+    public void answerSelection(View view) {
+        switch (view.getId()) {
+            case R.id.moreButton:
+                System.out.println("More");
+                processUserChoice(game.compare("G",A,B));
+                break;
+            case R.id.lessButton:
+                System.out.println("Less");
+                processUserChoice(game.compare("L",A,B));
+                break;
+        }
+    }
+
+    public void setData(){
+        if(A.getName()!="" && B.getName() !="") {
+            TextView scoreView = (TextView) findViewById(R.id.score);
+            scoreView.setText("Score: " + game.getScore().toString());
+
+            TextView name1 = (TextView) findViewById(R.id.name1);
+            name1.setText(A.getName());
+            TextView val1 = (TextView) findViewById(R.id.value1);
+
+
+            System.out.println("|||||||||||");
+            System.out.println(A.getData());
+            System.out.println(game.format(A.getData()));
+            System.out.println(B.getData());
+            System.out.println("|||}||||||");
+            val1.setText(beforeData+ game.format(A.getData())+afterData);
+
+            String imageUri = A.getUrl();
+            ImageView imgageOfA = (ImageView) findViewById(R.id.img1);
+            Picasso.get().load(imageUri).resize(400, 400).centerCrop().into(imgageOfA);
+
+            String imageUriB = B.getUrl();
+            ImageView imgageOfB = (ImageView) findViewById(R.id.img2);
+            Picasso.get().load(imageUriB).resize(400, 400).centerCrop().into(imgageOfB);
+            TextView name2 = (TextView) findViewById(R.id.name2);
+            name2.setText(B.getName());
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/finalproject/ModeActivity.java
+++ app/src/main/java/com/example/finalproject/ModeActivity.java
@@ -1,5 +1,7 @@
 package com.example.finalproject;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,5 +14,31 @@ public class ModeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mode2);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+    }
+
+    // Moving onto the game activity
+    public void modeSelect(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        switch (view.getId()) {
+            case R.id.netWorth:
+                System.out.println("Networth");
+                intent.putExtra("buttonId", "Networth");
+                break;
+            case R.id.youtubers:
+                System.out.println("Youtuber");
+                intent.putExtra("buttonId", "Youtube");
+                break;
+            case R.id.groceries:
+                System.out.println("Groc");
+                intent.putExtra("buttonId", "Grocery");
+                break;
+
+            case R.id.height:
+                System.out.println("h");
+                intent.putExtra("buttonId", "Heights");
+                break;
+        }
+
+        startActivity(intent);
     }
 }
